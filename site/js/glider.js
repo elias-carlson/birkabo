be5653d5656cc68c60c02a8806df09518827b382
elias-carlson <carlson.elias@gmail.com>
2019-12-29 14:27:48 +0100

    i might just be bad at this design stuff

--- site/js/glider.js
+++ site/js/glider.js
@@ -1,11 +1,10 @@
 new Glider(document.querySelector('.glider'), {
-    slidesToShow: 2.5,
-    slidesToScroll: 1,
-    scrollLock: true,
-    draggable: true,
-    dots: '#dots',
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    }
+    // slidesToShow: 1,
+    // // scrollLock: true,
+    // draggable: true,
+    // dots: '#dots',
+    // arrows: {
+    //     prev: '.glider-prev',
+    //     next: '.glider-next'
+    // }
 })
